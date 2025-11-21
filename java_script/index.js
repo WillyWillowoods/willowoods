5169d3e6f0939e274a86cbd30eccf3dc7a26313b
Laurens Vanderhoven <laurens.vanderhoven@thomasmore.be>
2025-11-21 21:05:00 +0900

    한줄 추가 했습니다. 주석입니다.

--- java_script/index.js
+++ java_script/index.js
@@ -3,3 +3,4 @@ console.log("Hello, JavaScript!");
 // 다시 commit 합니다.
 // new access tocken : glpat-YXM9-Kf3QPMlFhwPRfMxNW86MQp1OjEH.01.0w16i2sdi
 // tocken 은 창을 닫으면, 다시 알수 없단다..
+// http://localhost:8929
